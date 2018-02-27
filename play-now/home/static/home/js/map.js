cb26521e30889ccd32f9d4f434206ad5db8a6af9
thegajan <thegajannagaraj@gmail.com>
2018-02-26 23:50:00 -0800

    removed icon

--- play-now/home/static/home/js/map.js
+++ play-now/home/static/home/js/map.js
@@ -3,34 +3,31 @@ var map = new google.maps.Map(document.getElementById('map'), {
     lat: 37.7749295,
     lng: -122.4194155
   },
-  zoom: 6,
+  zoom: 15,
   disableDefaultUI: true,
   zoomControl: true
 });
-var infoWindow = new google.maps.InfoWindow({map: map});
+var infoWindow = new google.maps.InfoWindow({
+  map: map
+});
 
 // Try HTML5 geolocation.
 if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-        };
-
-        infoWindow.setPosition(pos);
-        infoWindow.setContent('Location found.');
-        map.setCenter(pos);
-    }, function() {
-        handleLocationError(true, infoWindow, map.getCenter());
-    });
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var pos = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    };
+    //                infoWindow.setPosition(pos);
+    //                infoWindow.setContent('Your Are Here.');
+    map.setCenter(pos);
+    map.setZoom(17);
+  }, function() {
+    handleLocationError(true, infoWindow, map.getCenter());
+  });
 } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
+  // Browser doesn't support Geolocation
+  handleLocationError(false, infoWindow, map.getCenter());
 }
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-                          'Error: The Geolocation service failed.' :
-                          'Error: Your browser doesn\'t support geolocation.');
-}
+var bounds = new google.maps.LatLngBounds();
+map.fitBounds(bounds);
