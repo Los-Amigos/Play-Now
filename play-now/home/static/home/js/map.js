9397d3a1cc523a208e1d49578e951cb19783345e
thegajan <thegajannagaraj@gmail.com>
2018-02-26 19:05:34 -0800

    added map and header

---
+++ play-now/home/static/home/js/map.js
@@ -0,0 +1,7 @@
+var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 37.7749295, lng: -122.4194155},
+    zoom: 6,
+    disableDefaultUI: true,
+    zoomControl: true
+});
+// var infoWindow = new google.maps.InfoWindow({map: map});
